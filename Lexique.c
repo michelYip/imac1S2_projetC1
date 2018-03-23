379746ff95e52ca8d057d8f199ecda96799c46e6
michelYip <wu.michel.yip@gmail.com>
2018-03-23 12:15:24 +0100

    TODO

--- Lexique.c
+++ Lexique.c
@@ -3,48 +3,82 @@
 
 int main(int argc, char ** argv){
 	Arbre dict;
-
+/*
 	printf("Creating dictionnary...\n");
 
-	createTreeFromFile(&dict, "test.txt");
+	createTreeFromText(&dict, "test.txt");
 	printf("Printing dictionnary...\n");
 	printDictionnary(dict);
+
+	printf("Creating Dictionnary from dict...\n");
+	createFileFromTree(dict, "dico", 1);
+	createFileFromTree(dict, "lexique", 0);
 
 	printf("End of program...\n");
 
 	return 1;
-
+*/
 /*
 	printf("Affichage de l'arbre lexicographique ... \n");
-	addWord(&dict, "Hola");
-	addWord(&dict, "àllo");
-	addWord(&dict, "Buonjuorno");
-	addWord(&dict, "Ni Hao"); 
-	addWord(&dict, "Namaste");
-	addWord(&dict, "Thank You");
-	addWord(&dict, "àrigato");
-	addWord(&dict, "Xie Xie");
-	addWord(&dict, "Bo Bun");
-	addWord(&dict, "Hello");
-	addWord(&dict, "Gracias");
-	addWord(&dict, "Kam sa");
-	addWord(&dict, "Ah Niong");
-	addWord(&dict, "été");
-	addWord(&dict, "à");
-	addWord(&dict, "ôla");
-	addWord(&dict, "è.é è.é");
-	addWord(&dict, "'");
-	printDictionnary(dict);
+	addWord(&dict, (unsigned char *)"ce");
+	addWord(&dict, (unsigned char *)"ces");
+	addWord(&dict, (unsigned char *)"des");
+	addWord(&dict, (unsigned char *)"le"); 
+	addWord(&dict, (unsigned char *)"les");
+	addWord(&dict, (unsigned char *)"lettre");
+	addWord(&dict, (unsigned char *)"mes");
+	addWord(&dict, (unsigned char *)"mettre");
+	printDictionnary(dict);	
 
-	printf("\"Guten Tag\" est présent dans le dictionnaire : %d\n",search(dict,"Guten Tag"));
-	printf("\"Bruh\" est présent dans le dictionnaire : %d\n",search(dict,"Bruh"));
-	
+	printf("Creating Dictionnary from dict...\n");
+	createFileFromTree(dict, "dico", 1);
 	return 1;
 */
+	/* TODO :*/
+	if (argc > 3 || argc < 2){
+		/*
+		Erreur : nombre de paramètre invalide
+		*/
+		fprintf(stderr, "Invalid number of argument ...\n");
+		exit(EXIT_FAILURE);
+	}
+	else if (argc == 2){
+		/*
+		Vérifier si le fichier argv[1] existe :
+			- le fichier existe, afficher le menu + traitement
+			- le fichier n'existe pas, quitter le programme
+		*/
+		if (access(argv[1], F_OK) != -1){
+			/* ---- file exist ---- */
+			printf("This file exist !\n");
+		} else {
+			printf("This file does not exist ! \n");
+			/* ---- file does not exist ---- */
+		}
+	}
+	else if (argc <= 3){
+		/*
+		Vérification de l'option :
+			si -S : 
+				reconstruction d'un arbre depuis argv[1] 
+				puis d'un fichier .DICO vers argv[1].DICO
+			sinon
+				si -r : 
+					reconstitution d'un arbre depuis argv[3].DICO s'il existe, argv[3] sinon 
+					traitement de recherche de argv[2] dans l'arbre
+				sinon : 
+					reconstitution d'un arbre depuis argv[2].DICO s'il existe, argv[2] sinon 
+					traitement 
+						si -l : affichage du lexique
+						si -s : sauvegarde du lexique vers argv[2].L
+		*/
+	}
+
+	return 1;
 } 
 
 /* Construit un arbre lexical depuis un fichier d'entrée */
-void createTreeFromFile(Arbre * tree, char * in){
+void createTreeFromText(Arbre * tree, char * in){
 	FILE * f = fopen(in, "r");
 	unsigned char str[MAXLENGTH];
 	int i = 0, j;
@@ -66,11 +100,48 @@ void createTreeFromFile(Arbre * tree, char * in){
 
 
 /* TODO : */
-/* Construit un lexique à partir d'un arbre lexical en entrée */
-void createFileFromTree(Arbre tree, char * out, int flag);
-
 /* Construit un fichier .L à partir de l'arbre en paramètre */
-void createLexique(FILE * f, Arbre tree);
+void createLexique(FILE * f, Arbre tree){
+	return;
+}
 
 /* Construit un fichier .DICO à partir de l'arbre en paramètre */
-void createDict(FILE * f, Arbre tree);
+void createDict(FILE * f, Arbre tree){
+	if(tree != NULL){
+		if (tree->lettre == '\0'){
+			fputc(' ', f);
+			createDict(f, tree->frered);
+		}
+		else {
+			fputc(tree->lettre, f);
+			createDict(f, tree->filsg);
+			createDict(f, tree->frered);
+		}
+	} else{
+		fputc('\n', f);
+	}
+}
+
+/* Construit un lexique à partir d'un arbre lexical en entrée */
+void createFileFromTree(Arbre tree, char * out, int flag){
+	char * filename;
+	FILE * f;
+	if ((filename = malloc(sizeof(char)*(strlen(out)+5))) == NULL){
+		fprintf(stderr, "Could not allocate memory in createFileFromTree()\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if (flag)
+		sprintf(filename, "%s.DICO", out);
+	else
+		sprintf(filename, "%s.L", out);
+
+	f = fopen(filename, "w");
+
+	if (flag)
+		createDict(f, tree);
+	else
+		createLexique(f, tree);
+
+	fclose(f);
+}
--- Lexique.h
+++ Lexique.h
@@ -2,13 +2,12 @@
 #ifndef __LEXIQUE__
 #define __LEXIQUE__
 
+#include <string.h>
+#include <unistd.h>
 #include "arbreLexical.h"
 
 /* Construit un arbre lexical depuis un fichier d'entrée */
-void createTreeFromFile(Arbre * tree, char * in);
-
-/* Construit un fichier .L ou .DICO à partir d'un arbre lexical en entrée */
-void createFileFromTree(Arbre tree, char * out, int flag);
+void createTreeFromText(Arbre * tree, char * in);
 
 /* TODO : */
 /* Construit un fichier .L à partir de l'arbre en paramètre */
@@ -16,5 +15,8 @@ void createLexique(FILE * f, Arbre tree);
 
 /* Construit un fichier .DICO à partir de l'arbre en paramètre */
 void createDict(FILE * f, Arbre tree);
+
+/* Construit un fichier .L ou .DICO à partir d'un arbre lexical en entrée */
+void createFileFromTree(Arbre tree, char * out, int flag);
 
 #endif
